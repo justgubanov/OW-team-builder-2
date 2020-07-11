7e0399be0d96ef1e4c873ac14b6fd220b33e4c02
Alex <gubanov1514@gmail.com>
2020-07-11 20:37:58 +0300

    Initial Commit

---
+++ OWTeamBuilder_2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  OWTeamBuilder_2
+//
+//  Created by Александр Губанов on 11.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OWTeamBuilder_2/OWTeamBuilder_2App.swift
@@ -0,0 +1,17 @@
+//
+//  OWTeamBuilder_2App.swift
+//  OWTeamBuilder_2
+//
+//  Created by Александр Губанов on 11.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct OWTeamBuilder_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
