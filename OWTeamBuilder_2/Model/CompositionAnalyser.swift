e53e9756f544b0dc3c8d3c0f8ed2e0c9f6b0eafb
Alex <gubanov1514@gmail.com>
2020-10-17 19:54:53 +0300

    Adopt package update

--- OWTeamBuilder_2/Model/CompositionAnalyser.swift
+++ OWTeamBuilder_2/Model/CompositionAnalyser.swift
@@ -14,23 +14,12 @@ struct CompositionAnalyser {
     var session: MatchSession
     
     func getCompositionValue(of hero: OWHero) -> Int {
-        guard let specificHero = HeroList[heroNumber: hero.number] else {
+        let heroId = HeroId(integerLiteral: hero.number)
+        guard let specificHero = HeroProvider.getHero(id: heroId) else {
             return 0
         }
-        
-        let enemies: [Hero] = session.enemySpots.compactMap { spot in
-            guard let hero = spot.hero else {
-                return nil
-            }
-            return HeroList[heroNumber: hero.number]
-        }
-
-        let allies: [Hero] = session.allySpots.compactMap { spot in
-            guard let hero = spot.hero else {
-                return nil
-            }
-            return HeroList[heroNumber: hero.number]
-        }
+        let enemies = getEnemies()
+        let allies = getAllies()
         
         let scoringBackground = ScoringBackground(allies: allies,
                                                   enemies: enemies,
@@ -38,5 +27,41 @@ struct CompositionAnalyser {
         
         let score = Scoring(of: specificHero, in: scoringBackground).result
         return Int(1000 * score.value)
+    }
+    
+    private func getAllies() -> [Hero] {
+        var heroes: [Hero]
+        
+        if session.focusedSpotType == .allied {
+            heroes = herHeroes(from: session.allySpots)
+        } else {
+            heroes = herHeroes(from: session.enemySpots)
+        }
+        
+        guard let selectedHero = session.focusedSpot?.hero,
+              let heroToRemove = HeroProvider.getHero(id: HeroId(integerLiteral: selectedHero.number)),
+              let removePosition = heroes.firstIndex(of: heroToRemove) else {
+            return heroes
+        }
+        heroes.remove(at: removePosition)
+        return heroes
+    }
+    
+    private func getEnemies() -> [Hero] {
+        if session.focusedSpotType == .allied {
+            return herHeroes(from: session.enemySpots)
+        } else {
+            return herHeroes(from: session.allySpots)
+        }
+    }
+    
+    private func herHeroes(from spots: [TeamSpot]) -> [Hero] {
+        return spots.compactMap { spot in
+            guard let hero = spot.hero else {
+                return nil
+            }
+            let heroId = HeroId(integerLiteral: hero.number)
+            return HeroProvider.getHero(id: heroId)
+        }
     }
 }
